8c0eaa9450f18c41f56eff0cf55d45e722c99668
satishlabs <spd61185@gmail.com>
2025-12-09 17:56:38 +0530

    Update self product

--- MS/src/com/blackhawk/ProductExceptSelf.java
+++ MS/src/com/blackhawk/ProductExceptSelf.java
@@ -1,26 +1,27 @@
 package com.blackhawk;
 
+import java.util.Arrays;
+
 public class ProductExceptSelf {
     static void main() {
         int[] nums = {1,2,3,4};
        int[] answer =  productExceptSelf(nums);
-       for(int i=0; i<answer.length; i++){
-           System.out.println(answer[i]);
-       }
+      for(int n: answer){
+          System.out.println(n);
+      }
     }
 
     private static int[] productExceptSelf(int[] nums) {
-        int[] answer = new int[nums.length];
-        answer[0] = 1;
-        for(int i=1; i<nums.length; i++){
-            answer[i] = answer[i-1]*nums[i-1];
-        }
-
-        int suffix = 1;
-        for(int i = nums.length-1; i>=0; i--){
-            answer[i] = answer[i]*suffix;
-            suffix*=nums[i];
-        }
-        return answer;
+      int n = nums.length;
+      int[] res = new int[n];
+       Arrays.fill(res,1);
+      for(int i=0; i<n; i++){
+          for(int j=0; j<n; j++){
+              if(i != j){
+                  res[i]*=nums[j];
+              }
+          }
+      }
+      return res;
     }
 }
