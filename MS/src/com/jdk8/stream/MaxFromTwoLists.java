95a1b01636f6db5ba5062c256bf62ca473b626d4
satishlabs <spd61185@gmail.com>
2025-12-10 20:11:30 +0530

    Find max element from two list

--- MS/src/com/jdk8/stream/MaxFromTwoLists.java
+++ MS/src/com/jdk8/stream/MaxFromTwoLists.java
@@ -1,19 +1,28 @@
 package com.jdk8.stream;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class MaxFromTwoLists {
     public static void main(String[] args) {
         List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
         List<Integer> list2 = Arrays.asList(6, 7, 8, 9, 10);
+        Optional<Integer> max = Stream.of(list1, list2)
+                .flatMap(List::stream)
+                .max(Integer::compareTo);
+        System.out.println("OverAll Max"+max.orElse(null));
 
-        Optional<Integer> max1 = list1.stream().max(Integer::compareTo);
-        Optional<Integer> max2 = list2.stream().max(Integer::compareTo);
 
-        Optional<Integer> overAllMax = max1.isPresent() && max2.isPresent() ? Optional.of(Math.max(max1.get(), max2.get()))
-                : max1.isPresent() ? max1 : max2;
-        overAllMax.ifPresent(max -> System.out.println("Max: "+max));
+        System.out.println("\n===========================");
+          Optional<Integer> secondMax = Stream.of(list1, list2)
+                    .flatMap(List::stream)
+                    .sorted(Comparator.reverseOrder())
+                    .skip(1)
+                    .findFirst();
+        System.out.println("Second Max: "+secondMax.orElse(null));
+
     }
 }
