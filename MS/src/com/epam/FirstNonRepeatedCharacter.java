cae57dc982b39eb18f66608132fbd3ff389545eb
satishlabs <spd61185@gmail.com>
2025-12-12 17:00:44 +0530

    Find first non repeated character

--- MS/src/com/epam/FirstNonRepeatedCharacter.java
+++ MS/src/com/epam/FirstNonRepeatedCharacter.java
@@ -1,11 +1,29 @@
 package com.epam;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FirstNonRepeatedCharacter {
     static void main() {
         String str = "satish";
+
+
+        System.out.println("\n=======================");
+        Optional<Character> first = str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(e ->e.getValue()==1)
+                .map(Map.Entry::getKey)
+                .findFirst();
+        System.out.println(first.orElse(null));
+
+        System.out.println("\n============================");
         Map<Character, Integer> map = new HashMap<>();
         for(Character c: str.toCharArray()){
             map.put(c, map.getOrDefault(c,0)+1);
