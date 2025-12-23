bd67e11135c8d8603afaf62058e2e3076752ad3f
satishlabs <spd61185@gmail.com>
2025-12-23 23:34:13 +0530

    LRU cache

---
+++ MS/src/com/globallogic/LRUCache.java
@@ -0,0 +1,31 @@
+package com.globallogic;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache <K, V> extends LinkedHashMap<K, V> {
+    private  final int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, .75f, true);
+        this.capacity = capacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size()>capacity;
+    }
+
+    static void main() {
+        LRUCache<Integer, String> cache = new LRUCache<>(3);
+        cache.put(1,"A");
+        cache.put(2, "B");
+        cache.put(3, "C");
+        System.out.println(cache);
+        cache.get(2);
+        System.out.println(cache);
+        cache.put(4, "D");
+        System.out.println(cache);
+
+    }
+}
