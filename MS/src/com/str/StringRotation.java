7b4eee57478da13d7917aa29ab8d4cf12d3bc5c3
satishlabs <spd61185@gmail.com>
2026-01-01 14:40:50 +0530

    String rotation

--- MS/src/com/str/StringRotation.java
+++ MS/src/com/str/StringRotation.java
@@ -1,10 +1,16 @@
 package com.str;
 
+import java.util.stream.IntStream;
+
 public class StringRotation {
     static void main() {
         String str1 = "abcd";
         String str2 = "cdab";
         System.out.println(isRotation(str1, str2));
+        System.out.println("\n=======================");
+        boolean isRotation = IntStream.range(0, str1.length())
+                .anyMatch(i ->(str1.substring(i)+str1.substring(0,i)).equals(str2));
+        System.out.println(isRotation);
     }
 
     private static boolean isRotation(String str1, String str2) {
