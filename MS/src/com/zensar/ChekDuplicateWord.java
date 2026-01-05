4d9eea21fcd086fb3961e5d055e8b0bc726e22ba
satishlabs <spd61185@gmail.com>
2026-01-05 17:39:09 +0530

    check duplicate word in string

---
+++ MS/src/com/zensar/ChekDuplicateWord.java
@@ -0,0 +1,24 @@
+package com.zensar;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ChekDuplicateWord {
+    static void main() {
+        String str = "Test Java With Java 8";
+        Map<String, Long> collect = Arrays.stream(str.split("\\s+"))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(word -> word, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue() > 1)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        if(collect.isEmpty()){
+            System.out.println("No duplicate found");
+        }else{
+            System.out.println("duplicate found:" +collect);
+        }
+    }
+}
