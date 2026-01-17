41858130e48e397e09315af3f106c242278eaa2b
satishlabs <spd61185@gmail.com>
2026-01-17 18:56:14 +0530

    Java based program

---
+++ MS/src/com/test/EmployeeObjectBased.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class EmployeeObjectBased {
+    static void main(String[] args) {
+       //1️⃣ Group employees by department
+        //2️⃣ Highest salary employee
+        //3️⃣ Average salary per department
+        //4️⃣ Sort employees by salary + name
+        //5️⃣ Salary greater than X
+        //6️⃣ Youngest / Oldest employee
+        ///7️⃣ Count employees per department
+        //8️⃣ Department with max employees
+        //9️⃣ Second highest salary employee
+        //0️⃣ List → Map (id, name)
+    }
+}
---
+++ MS/src/com/test/NumberBasedStream.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class NumberBasedStream {
+    static void main(String[] args) {
+       // 1️⃣ Duplicate numbers in a list
+        //2️⃣ Unique numbers
+        //3️⃣ Second highest number
+       //4️⃣ Max & Min value
+        //5️⃣ Sum of numbers
+        //6️⃣ Count even & odd numbers
+        //7️⃣ Sort ascending & descending
+        //8️⃣ Numbers starting with 1
+        //9️⃣ Remove duplicates
+        //0️⃣ Average of numbers
+    }
+}
