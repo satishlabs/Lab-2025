184cde7fe4d09bcbcc2ca78cffaef57dd03c7d9e
satishlabs <spd61185@gmail.com>
2026-01-21 17:15:01 +0530

    updated code for moving all zero end and start

--- MS/src/com/test/MoveAllZeroAt.java
+++ MS/src/com/test/MoveAllZeroAt.java
@@ -5,31 +5,35 @@ public class MoveAllZeroAt {
         int[] arr = {0, 1, 0, 3, 12};
         moveAllZeroAtEnd(arr);
         System.out.println("\n==============");
+        arr = new int[]{0, 1, 0, 3, 12};
         moveAllZeroAtStart(arr);
         }
-
-    private static void moveAllZeroAtStart(int[] arr) {
+    private static void moveAllZeroAtEnd(int[] arr) {
         int j = 0;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] != 0){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+        for(int i = 0; i< arr.length; i++){
+            if(arr[i] != 0) {
+                if (i != j) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
                 j++;
             }
         }
         for(int n: arr){
-            System.out.print(n+", ");
+            System.out.print(n+" ");
         }
     }
 
-    private static void moveAllZeroAtEnd(int[] arr) {
-        int j = arr.length;
-        for(int i = arr.length; i<=0; i++){
-            if(arr[i] == 0){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+    private static void moveAllZeroAtStart(int[] arr) {
+        int j = arr.length-1;
+        for(int i= arr.length-1; i>=0; i--){
+            if(arr[i] != 0) {
+                if (i != j) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
                 j--;
             }
         }
@@ -37,5 +41,7 @@ public class MoveAllZeroAt {
             System.out.print(n+", ");
         }
     }
+
+
 }
 
