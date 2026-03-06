1662ea83ca29822ed3786079621ac8335bbac450
satishlabs <spd61185@gmail.com>
2026-03-06 19:49:28 +0530

    updated code

--- MS/src/com/cba/GetErrorEvents.java
+++ MS/src/com/cba/GetErrorEvents.java
@@ -1,6 +1,7 @@
 package com.cba;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class GetErrorEvents {
@@ -16,5 +17,14 @@ public class GetErrorEvents {
                 .filter(e -> "ERROR".equals(e.level()))
                 .collect(Collectors.groupingBy(Event::service, Collectors.counting()))
                 .forEach((k,v) -> System.out.println(k + " : " + v));
+
+        System.out.println("\n============================");
+        eventList.stream()
+                .filter(e ->"ERROR".equals(e.level()))
+                .collect(Collectors.groupingBy(Event::service, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .ifPresent(System.out::println);
     }
 }
